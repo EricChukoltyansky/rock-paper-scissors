d0456f4ed1722db389a48de2452619ac118bb0d4
EricChukoltyansky <kazohelmet@gmail.com>
2022-08-10 16:02:02 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/EricChukoltyansky/rock-paper-scissors.git
+// git push -u origin main
