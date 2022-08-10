d8d07aba17838314b8e951368a57d5ecdbe28690
EricChukoltyansky <kazohelmet@gmail.com>
2022-08-10 20:07:01 +0300

    practice with react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/EricChukoltyansky/rock-paper-scissors.git
-// git push -u origin main
+import React, { useState } from "react";
+import Todos from "./Components/TodosList/Todos";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoName, setTodoName] = useState("");
+  const [todoCompleted, setTodoCompleted] = useState(false);
+
+  const handleName = () => {
+    setTodoName("Eric");
+  };
+
+  return (
+    <>
+      <Todos
+        name={todoName}
+        completed={todoCompleted}
+        onClick={() => handleName()}
+      ></Todos>
+    </>
+  );
+}
+
+export default App;
